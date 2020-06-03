4746fa338e235511ece89128c4038eb9f7bca133
dapeng <zhangpeng101@163.com>
2020-06-03 10:51:15 +0800

    antd-form表单新用法

--- src/pages/FormTest/index.js
+++ src/pages/FormTest/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form,Input ,Select} from 'antd';
+import { Form,Input ,Select, Button} from 'antd';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
@@ -7,6 +7,14 @@ const Option = Select.Option;
 const FormTest = () => {
 
     const [ form ] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log('values==>',values)
+    }
+
+    const onFinishFailed = (info) =>{
+        console.log('info==>',info)
+    }
 
     console.log('form',form)
 
@@ -19,10 +27,18 @@ const FormTest = () => {
                     aaaa:2222,
                     bbbb:"3"
                 }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
             >
                 <FormItem
                     name="aaaa"
                     label="号码" 
+                    rules={[
+                        {
+                            required:true,
+                            message:"必填！"
+                        }
+                    ]}
                  >
                     <Input placeholder="请输入参会号码" />
                 </FormItem>
@@ -34,6 +50,9 @@ const FormTest = () => {
                         <Option key="3">张三</Option>
                         <Option key="4">李四</Option>
                     </Select>
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" >提交</Button>
                 </FormItem>
             </Form>
         </div>
